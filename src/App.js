74082f08b982ca3b780cf35d25d207a6702696c6
ManukMamyan <manuk20061984@yandex.ru>
2019-09-09 19:04:13 +0300

    app without redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Pictures from './components/Pictures';
 import './App.css';
 
 class App extends Component {
@@ -10,52 +12,85 @@ class App extends Component {
     this.state = {
       width: 320,
       height: 0,
-      streaming: false
+      streaming: false,
+      pictures: []
     };
   }
 
   componentDidMount() {
-    const video = this.videoRef;
-    const canvas = this.canvasRef;
+    const video = this.videoRef.current;
 
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
       .then(function(stream) {
-        video.current.srcObject = stream;
-        video.current.play();
+        video.srcObject = stream;
+        video.play();
       })
       .catch(function(err) {
         console.log('An error occurred: ' + err);
       });
   }
 
+  setVideoParams = () => {
+    const video = this.videoRef.current;
+    const canvas = this.canvasRef.current;
+    const { height, width, streaming } = this.state;
+    if (!streaming) {
+      this.setState({
+        height: video.videoHeight / (video.videoWidth / width)
+      });
+
+      if (isNaN(height)) {
+        this.setState({
+          height: width / (4 / 3)
+        });
+      }
+
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      this.setState({
+        streaming: true
+      });
+    }
+  };
+
+  takePicture = () => {
+    const video = this.videoRef.current;
+    const canvas = this.canvasRef.current;
+    const { height, width, pictures } = this.state;
+
+    const context = canvas.getContext('2d');
+
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+    const data = canvas.toDataURL('image/png');
+    const newPictures = [...pictures, data];
+
+    this.setState({
+      pictures: newPictures
+    });
+  };
+
   render() {
+    const { pictures } = this.state;
+
     return (
       <div className='contentarea'>
-        <h1>MDN - WebRTC: Still photo capture demo</h1>
-        <p>
-          This example demonstrates how to set up a media stream using your
-          built-in webcam, fetch an image from that stream, and create a PNG
-          using that image.
-        </p>
+        <h1>Demo проект - фото с камеры</h1>
+        <p>Нажмите take photo, чтобы сфотографироваться.</p>
         <div className='camera'>
-          <video ref={this.videoRef} id='video'>
+          <video ref={this.videoRef} id='video' onCanPlay={this.setVideoParams}>
             Video stream not available.
           </video>
-          <button id='startbutton'>Take photo</button>
+          <button id='startbutton' onClick={this.takePicture}>
+            Take photo
+          </button>
         </div>
         <canvas id='canvas' ref={this.canvasRef}></canvas>
-        <div className='output'>
-          <img id='photo' alt='The screen capture will appear in this box.' />
-        </div>
-        <p>
-          Visit our article{' '}
-          <a href='https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos'>
-            {' '}
-            Taking still photos with WebRTC
-          </a>{' '}
-          to learn more about the technologies used here.
-        </p>
+        <Pictures pictures={pictures} />
       </div>
     );
   }
---
+++ src/components/Pictures.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Pictures = props => {
+  const { pictures } = props;
+
+  const takenPicturesElem = pictures.map((picture, idx) => {
+    return (
+      <img
+        key={idx}
+        src={picture}
+        class='photo'
+        alt='The screen capture will appear in this box.'
+      />
+    );
+  });
+  return <div className='output'>{takenPicturesElem}</div>;
+};
+
+export default Pictures;
